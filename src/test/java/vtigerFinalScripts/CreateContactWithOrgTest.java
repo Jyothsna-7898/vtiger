2c84d8aeb0b8ad41a264034368fcca5e7a2a0909
Jyothsna7898 <139970874+Jyothsna7898@users.noreply.github.com>
2023-08-26 09:07:07 +0530

    Framework first commit

---
+++ src/test/java/vtigerFinalScripts/CreateContactWithOrgTest.java
@@ -0,0 +1,36 @@
+package vtigerFinalScripts;
+
+import java.util.Map;
+
+import org.testng.annotations.Test;
+import org.testng.asserts.SoftAssert;
+
+import genericlibraries.BaseClass;
+import genericlibraries.IConstantPath;
+
+public class CreateContactWithOrgTest extends BaseClass {
+	@Test
+	public void createContactWithOrgTest() {
+		SoftAssert soft = new SoftAssert();
+		home.clickContacts();
+		soft.assertTrue(driver.getTitle().contains("Contacts"));
+
+		contacts.clickPlusButton();
+		soft.assertEquals(createContact.getPageHeader(), "Creating New Contact");
+
+		Map<String, String> map = excel.readFromExcel("Sheet2", "Create Contact");
+		String lastName = map.get("Last Name") + jutil.generateRandomNum(58);
+		createContact.setLastName(lastName);
+		createContact.selectExistingorg(web, map.get("Organization Name"));
+		createContact.clickSaveButton();
+		soft.assertTrue(newContactInfo.getPageHeader().contains(lastName));
+
+		if (newContactInfo.getPageHeader().contains(lastName)) 
+			excel.writeToExcel("Sheet2", "Create Contact With Organization", "Pass", IConstantPath.Excel_Path);
+		else 
+			excel.writeToExcel("Sheet2", "Create Contact With Organization", "Fail", IConstantPath.Excel_Path); 
+		
+		soft.assertAll();
+	}
+}
+
